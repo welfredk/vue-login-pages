00fe03a3e9e21b68d98dc525a9ad98ee59cb00b2
welfredk <welfredkng@gmail.com>
2023-06-07 12:10:23 +0800

    Fix line ending issues

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // base: '/vue-login-pages/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-login-pages/'
+    : '/'
 })
